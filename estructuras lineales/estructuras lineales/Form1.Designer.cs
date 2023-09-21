927cac772977506d0b0ba259f0b5359df0ddbe90
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-09-21 17:21:33 -0600

    Add files via upload

---
+++ estructuras lineales/estructuras lineales/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace estructuras_lineales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AgregarElemento = new System.Windows.Forms.Button();
+            this.EliminarElemento = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // AgregarElemento
+            // 
+            this.AgregarElemento.Location = new System.Drawing.Point(32, 13);
+            this.AgregarElemento.Name = "AgregarElemento";
+            this.AgregarElemento.Size = new System.Drawing.Size(75, 23);
+            this.AgregarElemento.TabIndex = 0;
+            this.AgregarElemento.Text = "button1";
+            this.AgregarElemento.UseVisualStyleBackColor = true;
+            this.AgregarElemento.Click += new System.EventHandler(this.AgregarElemento_Click);
+            // 
+            // EliminarElemento
+            // 
+            this.EliminarElemento.Location = new System.Drawing.Point(140, 13);
+            this.EliminarElemento.Name = "EliminarElemento";
+            this.EliminarElemento.Size = new System.Drawing.Size(75, 23);
+            this.EliminarElemento.TabIndex = 1;
+            this.EliminarElemento.Text = "button1";
+            this.EliminarElemento.UseVisualStyleBackColor = true;
+            this.EliminarElemento.Click += new System.EventHandler(this.EliminarElemento_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(44, 73);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "button1";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(161, 73);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "button2";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(32, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(367, 260);
+            this.listBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(461, 210);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(495, 263);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.EliminarElemento);
+            this.Controls.Add(this.AgregarElemento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AgregarElemento;
+        private System.Windows.Forms.Button EliminarElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ estructuras lineales/estructuras lineales/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace estructuras_lineales
+{
+    public partial class Form1 : Form
+    {
+        // Definimos la estructura de un nodo de la lista enlazada
+        private class Nodo
+        {
+            public int Valor;
+            public Nodo Siguiente;
+
+            public Nodo(int valor)
+            {
+                Valor = valor;
+                Siguiente = null;
+            }
+        }
+
+        private Nodo cabeza; // El primer nodo de la lista enlazada
+        public Form1()
+        {
+            InitializeComponent();
+            cabeza = null; // Inicialmente, la lista está vacía
+        }
+
+        private void AgregarElemento_Click(object sender, EventArgs e)
+        {
+            Nodo nuevoNodo = new Nodo(valor);
+
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+            }
+            else
+            {
+                Nodo nodoActual = cabeza;
+                while (nodoActual.Siguiente != null)
+                {
+                    nodoActual = nodoActual.Siguiente;
+                }
+                nodoActual.Siguiente = nuevoNodo;
+            }
+        }
+
+        private void EliminarElemento_Click(object sender, EventArgs e)
+        {
+            if (cabeza == null)
+            {
+                MessageBox.Show("La lista está vacía.");
+                return;
+            }
+
+            if (cabeza.Valor == valor)
+            {
+                cabeza = cabeza.Siguiente;
+                return;
+            }
+
+            Nodo nodoActual = cabeza;
+            while (nodoActual.Siguiente != null)
+            {
+                if (nodoActual.Siguiente.Valor == valor)
+                {
+                    nodoActual.Siguiente = nodoActual.Siguiente.Siguiente;
+                    return;
+                }
+                nodoActual = nodoActual.Siguiente;
+            }
+
+            MessageBox.Show("El elemento no se encontró en la lista.");
+        }
+        // Función para mostrar los elementos de la lista en un ListBox
+        private void MostrarLista()
+        {
+            listBox1.Items.Clear();
+            Nodo nodoActual = cabeza;
+            while (nodoActual != null)
+            {
+                listBox1.Items.Add(nodoActual.Valor);
+                nodoActual = nodoActual.Siguiente;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int valor = Convert.ToInt32(textBox1.Text);
+            AgregarElemento(valor);
+            MostrarLista();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int valor = Convert.ToInt32(textBox2.Text);
+            EliminarElemento(valor);
+            MostrarLista();
+        }
+    }
+}
